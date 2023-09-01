3c0100db6b63a922fe9f76902402806da2da2574
Maria Luiza <malusraimundo@gmail.com>
2023-09-01 17:25:45 -0300

    código da aula adicionado

---
+++ Laboratorio/Aula3/app_imc/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+	let peso = 0;
+	let altura = 0;
+	let imc = 0;
+
+	function calcularIMC() { 
+		if(peso > 0 && altura > 0) {
+			imc = (peso / Math.pow(altura,2)).toFixed(2);
+		}
+		else imc = 0;
+	}
+</script>
+
+<main>
+	<h1>Calculadora de IMC</h1>
+	<label>
+		Peso (kg): <input type="number" bind:value={peso} on:input={calcularIMC}>
+	</label>
+	<label>
+		Altura (m): <input type="number" bind:value={altura} on:input={calcularIMC}>
+	</label>
+	{#if imc > 0}
+		<p>Seu IMC é {imc}</p>
+	{/if}
+</main>
+
+<style>
+	main {
+		font-family: sans-serif;
+		text-align: center;
+		padding: 2em;
+	}
+
+	h1 {
+		font-size: 1.5em;
+		margin-bottom: 1rem;
+	}
+
+	label {
+		display: block;
+		margin-bottom: 0.5rem;
+	}
+
+	input {
+		padding: 0.25rem;
+	}
+</style>
