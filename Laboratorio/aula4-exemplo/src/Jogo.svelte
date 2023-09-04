8878166e703c7a708d12fe1c7946f24780c26c4d
Maria Luiza <malusraimundo@gmail.com>
2023-09-04 18:48:15 -0300

    desafio aula 4 feito

---
+++ Laboratorio/aula4-exemplo/src/Jogo.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Jogador from "./Jogador.svelte";
+	import Swal from "sweetalert2";
+
+	function marcaGol() {
+		console.log("Marcou 1 GOL");
+		Swal.fire({
+			icon: "success",
+			title: "Gol!",
+			text: "Você marcou um gol!",
+		});
+	}
+</script>
+
+<main>
+	<h1>Exibe a informação quando marca GOL</h1>
+	<Jogador acao={marcaGol} />
+</main>
