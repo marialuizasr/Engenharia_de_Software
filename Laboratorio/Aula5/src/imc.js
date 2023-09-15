fb6a40e2b3d097feeaf2a04a37e8d5395b8a0209
Maria Luiza <malusraimundo@gmail.com>
2023-09-15 12:08:23 -0300

    desafio da aula 5 finalizado

--- Laboratorio/Aula5/src/imc.js
+++ Laboratorio/Aula5/src/imc.js
@@ -8,4 +8,31 @@ function calcularIMC(peso, altura) {
     return ((peso / (alturaMetros * alturaMetros)).toFixed(2));
 }
 
-module.exports = { calcularIMC };
+function imcSaudavel(peso, altura) {
+    const imc = calcularIMC(peso, altura);
+    return imc >= 18.5 && imc <= 24.9;
+}
+
+function classificarIMC(peso, altura) {
+    const imc = calcularIMC(peso, altura);
+
+    if (imc < 16) {
+        return 'Magreza grave';
+    } else if (imc >= 16 && imc < 17) {
+        return 'Magreza moderada';
+    } else if (imc >= 17 && imc < 18.5) {
+        return 'Magreza leve';
+    } else if (imc >= 18.5 && imc < 25) {
+        return 'Peso normal';
+    } else if (imc >= 25 && imc < 30) {
+        return 'Sobrepeso';
+    } else if (imc >= 30 && imc < 35) {
+        return 'Obesidade Grau I';
+    } else if (imc >= 35 && imc < 40) {
+        return 'Obesidade Grau II (severa)';
+    } else {
+        return 'Obesidade Grau III (mórbida)';
+    }
+}
+
+module.exports = { calcularIMC, imcSaudavel, classificarIMC };
--- Laboratorio/Aula5/test/imc.test.js
+++ Laboratorio/Aula5/test/imc.test.js
@@ -1,23 +1,41 @@
 const { expect } = require('chai');
-const { calcularIMC } = require('../src/imc');
+const { calcularIMC, imcSaudavel, classificarIMC } = require('../src/imc');
 
 describe('Calculadora de IMC', () => {
     it('Calculadora deve rodar tranquilo', () => {
         const peso = 80;
         const altura = 180;
 
-        const ImcEsperado = (peso / Math.pow(altura / 100, 2)).toFixed(2);
-        const ImcCalculado = calcularIMC(peso, altura);
+        const imcEsperado = (peso / Math.pow(altura / 100, 2)).toFixed(2);
+        const imcCalculado = calcularIMC(peso, altura);
 
-        expect(ImcCalculado).to.equal(ImcEsperado);
+        expect(imcCalculado).to.equal(imcEsperado);
     });
 
     it('altura = 0, entrada inadequada', () => {
         const peso = 70;
         const altura = 0;
 
-        const IMCCalculado = calcularIMC(peso, altura);
+        const imcCalculado = calcularIMC(peso, altura);
 
-        expect(IMCCalculado).to.equal('NaN');
+        expect(imcCalculado).to.equal('NaN');
     })
+
+    it('Avaliar se o IMC é saudável', () => {
+        const peso = 70;
+        const altura = 170;
+
+        const resultado = imcSaudavel(peso, altura);
+
+        expect(resultado).to.be.true;
+    });
+
+    it('Classificar o IMC', () => {
+        const peso = 90;
+        const altura = 175;
+
+        const categoria = classificarIMC(peso, altura);
+
+        expect(categoria).to.equal('Sobrepeso');
+    });
 });
